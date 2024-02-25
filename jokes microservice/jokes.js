98b71acef0fd7fb78588b3caa39b8592704ace90
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-02-25 12:10:19 +0000

    Implemented fetching jokes and types from the database

--- jokes microservice/jokes.js
+++ jokes microservice/jokes.js
@@ -40,31 +40,64 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
+app.get('/type', async(req, res) => {
+    try
+    {
+        const result = await getTypes();
+        res.send(result);
+    }
+    catch(err)
+    {
+        res.status(500).send(err);
+    }
+})
+
+app.get('/joke', async(req, res) => {
+    try
+    {
+        const result = await getJokes();
+        res.send(result);
+    }
+    catch(err)
+    {
+        res.status(500).send(err);
+    }
+})
+
+async function getJokes() {
+    const result = new Promise((resolve, reject) => {
+        const sql = `select * from jokes`;
+        db.query(sql, (err, results) => {
+            if (err)
+            {
+                reject(`Database error: ${err.message}`);
+            }
+            else
+            {
+                resolve(results);
+            }
+        })
+    })
+    return result;
+}
+
+async function getTypes(){
+    const result = new Promise((resolve, reject) => {
+        const sql = `select * from joke_types`;
+        db.query(sql, (err, results) => {
+            if (err)
+            {
+                reject(`Database error: ${err.message}`);
+            }
+            else
+            {
+                resolve(results);
+            }
+        })
+    })
+    return result;
+}
+
 app.listen(PORT, () => {
     console.log(`Joke service listening on port ${PORT}`);
 })
-
-// Example endpoint to fetch types
-app.get('/type', (req, res) => {
-
-    console.log(`Fetching Jokes succeeded`);
-    // Fetch jokes from database
-
-    res.json(["general", "test"]); // example response
-})
-
-// Example endpoint to fetch jokes
-app.get('/joke', (req, res) => {
-
-    const {type, count} = req.query;
-    console.log(`Fetching ${count || 1} joke(s) of type '${type || "any"}' succeeded`);
-    // Scaffold code
-    // Write the actual code to fetch a joke by type and count
-
-    res.json({
-        jokes: [
-            { id: 1, type: type || "any", joke: "My therapist says I have a preoccupation with vengeance. We’ll see about that." }
-        ]
-    });
-});
-
