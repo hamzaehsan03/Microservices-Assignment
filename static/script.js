634589067e349372bb2dee2c312fef304cae468a
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-02-26 07:46:23 +0000

    Fixed a bug where the dropdown selection wouldn't stay selected

--- static/script.js
+++ static/script.js
@@ -14,6 +14,9 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             const types = await response.json();
+
+            // Remember the current selection for the dropdown
+            const currentSelection = jokeTypeSelect.value;
             jokeTypeSelect.innerHTML = '<option value="any">Any</option>'; // Reset and add default 'any' option
             
             types.forEach(type => {
@@ -22,6 +25,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 option.textContent = type.type_name.charAt(0).toUpperCase() + type.type_name.slice(1);
                 jokeTypeSelect.appendChild(option);
             });
+
+            // Revert back to the selection
+            if (jokeTypeSelect.querySelector(`option[value="${currentSelection}"]`)) 
+            {
+                jokeTypeSelect.value = currentSelection;
+            }
         } 
 
         catch (error) 
