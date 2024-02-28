75003c3e8130c22d56b06edf12023f4cebfad346
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-02-28 10:56:55 +0000

    Created docker compose files Containerised SQL database The main program and the database still don't communicate between containers, and I'm not too sure why dump.sql is just something to import into the container

---
+++ dump.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: jokes_service
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `joke_types`
+--
+
+DROP TABLE IF EXISTS `joke_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `joke_types` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `type_name` (`type_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `joke_types`
+--
+
+LOCK TABLES `joke_types` WRITE;
+/*!40000 ALTER TABLE `joke_types` DISABLE KEYS */;
+INSERT INTO `joke_types` VALUES (1,'general'),(3,'knock-knock'),(2,'programming');
+/*!40000 ALTER TABLE `joke_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jokes`
+--
+
+DROP TABLE IF EXISTS `jokes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jokes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type_id` int DEFAULT NULL,
+  `joke_text` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_joke_type` (`type_id`),
+  CONSTRAINT `fk_joke_type` FOREIGN KEY (`type_id`) REFERENCES `joke_types` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jokes`
+--
+
+LOCK TABLES `jokes` WRITE;
+/*!40000 ALTER TABLE `jokes` DISABLE KEYS */;
+INSERT INTO `jokes` VALUES (1,1,'Why don’t scientists trust atoms? Because they make up everything.'),(2,2,'Why do programmers prefer dark mode? Because light attracts bugs.'),(3,3,'Knock knock. Who’s there? A broken pencil. A broken pencil who? Never mind, it’s pointless.');
+/*!40000 ALTER TABLE `jokes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-28 10:16:56
