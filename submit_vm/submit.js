6844399c1e1434ba38826c378bab22b4dd096801
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-03-08 15:21:46 +0000

    Implemented basic documentation for submit Implemented for /sub

--- submit_vm/submit.js
+++ submit_vm/submit.js
@@ -4,11 +4,30 @@ const amqp = require('amqplib');
 const app = express();
 const path = require('path')
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cors());
 
 const PORT = process.env.SUBMIT_PORT || 3200
+
+const options = {
+    definition: {
+
+        openapi: '3.0.0',
+        info: {
+            title: 'Submit Service',
+            version: '1.0.0',
+            description: 'Documentation for Submit Service',
+        },
+    },
+    apis: ['submit.js']
+};
+
+const swaggerDocs = swaggerJsDoc(options);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 app.get(['/', 'index.html'], (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'index.html'))
@@ -39,6 +58,31 @@ app.get('/types', async (req, res) => {
     
 })
 
+/**
+ * @swagger
+ * /sub:
+ *   post:
+ *     summary: Submits a new joke
+ *     description: Submit a new joke to the SUBMITTED_JOKES queue
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               type:
+ *                 type: string
+ *                 description: Type of the joke
+ *               jokeText:
+ *                 type: string
+ *                 description: The content of the joke
+ *     responses:
+ *       200:
+ *         description: Joke submitted successfully
+ *       500:
+ *         description: Failed to submit joke
+ */
 
 app.post('/sub', async (req, res) => {
     const {type, jokeText} = req.body;
