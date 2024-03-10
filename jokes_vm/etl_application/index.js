f995239c9276d17a712934413df88b341d5e7d40
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-03-10 18:09:57 +0000

    skeleton implementation of moderator service

--- jokes_vm/etl_application/index.js
+++ jokes_vm/etl_application/index.js
@@ -33,7 +33,7 @@ async function getTypeID(typeName) {
 
 async function consumeMessage() {
     try {
-        const conn = await amqp.connect('amqp://admin:admin@10.0.0.6');
+        const conn = await amqp.connect(process.env.RABBITMQ_SUBMIT_IP);
         const channel = await conn.createChannel();
 
         await channel.assertQueue(queue, { durable: false });
