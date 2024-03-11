b2bd0599b505b0202787e9dffaf1cb63f99f3d75
hamzaehsan03 <153811806+hamzaehsan03@users.noreply.github.com>
2024-03-11 19:00:09 +0000

    Implemented consuming messages from SUBMITTED_JOKES queue

--- moderator_vm/mod.js
+++ moderator_vm/mod.js
@@ -26,6 +26,44 @@ app.get('/types', async (req, res) => {
 
 })
 
+app.get('/mod', async (req, res) => {
+
+    try 
+    {
+        const conn = await amqp.connect(process.env.RABBITMQ_SUBMISSION_IP);
+        console.log(`Connected to RabbitMQ`);
+
+        const channel = await conn.createChannel();
+        await channel.assertQueue('SUBMITTED_JOKES', { durable: true });
+
+        channel.get('SUBMITTED_JOKES', {}, (err, message) => {
+            if (err) 
+            {
+                console.error(`Failed to retrieve message from SUBMITTED_JOKES: ${err}`);
+                res.status(500).send('Failed to retrieve joke');
+                return;
+            }
+            if (message) 
+            {
+                const joke = JSON.parse(message.content.toString());
+                channel.ack(message);
+                res.json(joke);
+            } 
+            else 
+            {
+                // No message was available at the time of the request
+                res.status(404).send({ message: "No jokes available for moderation." });
+            }
+        });
+    } 
+    catch (error) 
+    {
+        console.error(`Failed to connect to RabbitMQ: ${error}`);
+        res.status(500).send('Failed to retrieve');
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 });
